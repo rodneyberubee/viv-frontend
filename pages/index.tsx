c46659b158ce076b8694b0750b82a6187401e7b1
rodneyberubee <rodney@vivaitable.com>
2025-08-05 20:54:15 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage = () => {
           <nav className="space-x-6">
             <Link href="#features" className="text-gray-700 hover:text-orange-500">Features</Link>
             <Link href="#pricing" className="text-gray-700 hover:text-orange-500">Pricing</Link>
-            <Link href="#contact" className="text-gray-700 hover:text-orange-500">Contact</Link>
+            <Link href="/contact" className="text-gray-700 hover:text-orange-500">Contact</Link>
             <Link href="/login" className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600">
               Sign In
             </Link>
