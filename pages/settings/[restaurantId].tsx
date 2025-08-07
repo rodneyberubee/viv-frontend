b35ba8fb52b4dbdbdb7057909dae7a5a55a51798
rodneyberubee <rodney@vivaitable.com>
2025-08-06 19:51:04 -0700

    Create [restaurantId].tsx

---
+++ pages/settings/[restaurantId].tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import SettingsPage from '../../components/SettingsPage'; // adjust path if needed
+
+const SettingsDynamic = () => {
+  const router = useRouter();
+  const { restaurantId } = router.query;
+
+  if (typeof restaurantId !== 'string') {
+    return <div className="p-8 text-center">Loading...</div>;
+  }
+
+  return <SettingsPage restaurantId={restaurantId} />;
+};
+
+export default SettingsDynamic;
